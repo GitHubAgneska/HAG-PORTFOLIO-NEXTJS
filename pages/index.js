3c8759be09040c95b9488a7fd5c6b3360b7cb52d
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-02-08 10:46:34 +0100

    +meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
-
+import previewImage from '../public/imgs/preview.png'
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>HAG portfolio 2022</title>
-        <meta name="description" content="made with NextJS" />
+        <meta name="description" content="HAG portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={previewImage} key="ogimage" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
